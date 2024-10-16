3f74b1f32179c99d19610b21a1b308df540eec85
Serhat Karaarslan <sk019655@fh-muenster.de>
2024-10-16 22:28:50 +0200

    OOP

---
+++ OOP/oop.py
@@ -0,0 +1,24 @@
+#Object Oriented Programming
+
+#class
+#instance (object)
+
+
+#<class 'list'> Hier our lst is an instance(object) from list class
+#when i create an object from a class, i actually creaze a copy from this class with the properties, methodes. atributes from this before defined class
+lst = [1,2,3]
+result = type(lst)
+print(result)   
+
+#hier ist the type again <class 'list'>
+lst2 = [1,2,3,4,5,6]
+result = type(lst2) 
+print(result)  
+
+# we can use the methods with of this class with the "." 
+#lst2. => then i see the defined methods and can use these mehtods of this class 
+
+#I can create my class too
+ 
+
+
