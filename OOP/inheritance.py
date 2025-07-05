ca12f4798fd8659974dbaf9e64528f9d063e3039
Serhat Karaarslan <sk019655@fh-muenster.de>
2025-07-05 16:40:27 +0200

    Inheritance in pyhton theorie

---
+++ OOP/inheritance.py
@@ -0,0 +1,21 @@
+# Inheritance is a fundamental concept in object-oriented programming that allows a class (the child or derived class) to inherit
+
+# Student
+    #name, surname, no
+    #study()
+    #eat()
+
+# Teacher
+    #name, surname, department
+    #teach()
+    #eat()
+
+#Some attributes or some methods are common in different classes. Because of that we can create a class, which has the common attributes or methods
+
+#Person
+    #name,surname, address
+    #eat()
+
+#Person is now base/parent class and the other class, which inherits from parent class are child classes.
+# Thank inheritance we dont need create common object attributes or methods in child classes.
+#We have them in parent class. Thats very practical and spare time and code line.
